3e018ef63855cc44c89f4ef128416e0aecc16683
Nathan Lee <hungryballer@gmail.com>
2018-05-23 13:48:36 -0700

    First commit

---
+++ MyBartender/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyBartender
+//
+//  Created by Nathan Lee on 5/23/18.
+//  Copyright © 2018 Lee. All rights reserved.
+//  hehe
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
